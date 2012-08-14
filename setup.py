d273f0305fbc5663ef42d2bfef2bf3888bfb3923
Ales Zabala Alava (Shagi) <shagi@gisa-elkartea.org>
2012-08-15 01:19:44 +0200

    more fixes for git migration

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.5'
 setup(name='collective.portlet.contentleadimage',
       version=version,
       description="Collection portlet that shows contenteleadimages",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -19,7 +19,7 @@ setup(name='collective.portlet.contentleadimage',
       keywords='',
       author='Ales Zabala Alava (Shagi)',
       author_email='shagi@gisa-elkartea.org',
-      url='http://github.com/collective/collective.portlet.contentleadimage'
+      url='http://github.com/collective/collective.portlet.contentleadimage',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.portlet'],
